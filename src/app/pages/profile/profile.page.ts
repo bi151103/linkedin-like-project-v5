f04b0b514be911518af65bdbda41b640fd8320b8
bi151103 <dangphanminhphuctbag@gmail.com>
2026-05-23 20:07:53 +0700

    feat: add background image section

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -4,6 +4,7 @@ import OverlayDirective from '../../components/overlay/overlay.component';
 import DialogComponent from '../../components/dialog/dialog.component';
 import SearchComboboxDialogComponent from '../../components/search-combobox-dialog/search-combobox-dialog.component';
 import FooterComponent from '../../components/footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -13,11 +14,25 @@ import FooterComponent from '../../components/footer/footer.component';
     DialogComponent,
     SearchComboboxDialogComponent,
     FooterComponent,
+    RouterLink,
   ],
   template: `
     <app-header
       (onShowSearchComboboxDialog)="showSearchComboboxDialog()"
     ></app-header>
+    <ng-container>
+      <section class="relative">
+        <img
+          src="assets/images/background-image-cut.jpg"
+          class="w-full bg-white align-middle"
+        />
+        <a routerLink="/my-preference"
+          ><img
+            src="assets/images/icons8-setting-100.png"
+            class="p-5px absolute top-0 right-0 mt-[15px] mr-[15px] aspect-square w-[25px] rounded-full bg-white"
+        /></a>
+      </section>
+    </ng-container>
     <app-footer></app-footer>
     <div appOverlay>
       <app-dialog
@@ -28,6 +43,9 @@ import FooterComponent from '../../components/footer/footer.component';
       </app-dialog>
     </div>
   `,
+  host: {
+    class: 'block py-50px',
+  },
 })
 export class ProfilePage {
   searchDialogVisible = signal(false);
